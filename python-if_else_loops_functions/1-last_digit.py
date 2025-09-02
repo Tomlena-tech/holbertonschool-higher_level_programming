5c856d7c73b589e79dbf3d22853a00bab8d509f9
Tomlena-tech <decourttthomas@gmail.com>
2025-09-02 15:31:39 +0200

    Add task 0 & 1 loop if etc

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0
+#je cherche le dernier chiffre
+dernier_chiffre = int(str(number)[-1])
+print(f"Dernier chiffre de {number} est {dernier_chiffre}", end="")
