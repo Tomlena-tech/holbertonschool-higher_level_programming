e8d387339d06b97823e91422413fa46d148e5a8f
Tomlena-tech <decourttthomas@gmail.com>
2025-09-03 11:03:22 +0200

    updat etask 1 with last digit & modulo

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 
-#je cherche le dernier chiffre
-last_number = int(str(number)[-1])
-print(f"Last digit of {number} is {last_number}", end="")
-if last_number >5:
+#je cherche le dernier chiffre avec modulo qui conserve le signe
+last_digit = number % 10 
+print(f"Last digit of {number} is {last_digit}", end="")
+
+if last_digit >5:
     print(" and is greater than 5")
-elif last_number == 0:
+elif last_digit == 0:
     print(" and is 0")
-elif last_number < 6 and last_number != 0:
+else:
     print(" and is less than 6 and not 0")
