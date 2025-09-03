7000d83880af478b1a1f1dff7a158655160e1da3
Tomlena-tech <decourttthomas@gmail.com>
2025-09-03 11:09:58 +0200

    add debug task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,7 @@ number = random.randint(-10000, 10000)
 
 #je cherche le dernier chiffre avec modulo qui conserve le signe
 last_digit = number % 10 
+print(f"\nDEBUG: {number} % 10 = {last_digit}")
 print(f"Last digit of {number} is {last_digit}", end="")
 
 if last_digit >5:
