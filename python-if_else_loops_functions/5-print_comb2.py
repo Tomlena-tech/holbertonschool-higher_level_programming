aebe94da57173edf6ff7abeb9a907897d16b2fc7
Tomlena-tech <decourttthomas@gmail.com>
2025-09-04 12:16:28 +0200

    update task 5 delete 1 print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0,99):
-    if number <10:
-        print("0{:d}".format(number), end=", ")
-    else: print("{:d}".format(number), end=", ")
-print("99")
+    if number <100:
+        print("{:02d}".format(number), end=", ")
+print("99")        
+        
