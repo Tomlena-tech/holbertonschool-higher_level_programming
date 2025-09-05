7d94e933f84c249b7d8dcc1c048feffdf5f98b1e
Tomlena-tech <decourttthomas@gmail.com>
2025-09-05 12:02:53 +0200

    Add task 0 completed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-result = add(1, 2)
-print("{} + {} = {}".format(1, 2, result)) 
+a = 1
+b = 2
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result)) 
