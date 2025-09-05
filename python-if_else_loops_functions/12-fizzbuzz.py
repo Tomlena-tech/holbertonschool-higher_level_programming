cbd208034e142a6ab5fea4ee5af4b6797f4cb424
Tomlena-tech <decourttthomas@gmail.com>
2025-09-05 11:10:16 +0200

    Add correct indentation Task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        print("Fizz" * (number % 3 == 0) + "Buzz" * (number % 5 == 0) or number, end=" ")
+        print("Fizz" * (number % 3 == 0) + "Buzz" * (number % 5 == 0) or number,
+              end=" ")
