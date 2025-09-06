5563aa06bcb196227bede7298e0928897b300c11
Tomlena-tech <decourttthomas@gmail.com>
2025-09-06 15:33:35 +0200

    Add atsk 2 part 1 no too bad

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    len_argv = len(argv) - 1
+
+if len_argv == 0:
+    print("0 arguments.")
+elif len_argv == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(len_argv))     
