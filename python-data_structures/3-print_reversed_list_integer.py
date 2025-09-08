ba88afe23e4029a3cad0f7391d421622c294ff24
Tomlena-tech <decourttthomas@gmail.com>
2025-09-08 12:51:27 +0200

    Add test on task 3 ésd method ::-1

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list):
-    for i in a range(len(my_list))
+def print_reversed_list_integer(my_list=[]):
+    for element in my_list[::-1]:
+        print("{}".format(element))
