f1beb53a9a8b835fce01711f5bc6b89f440122c4
Tomlena-tech <decourttthomas@gmail.com>
2025-09-08 11:59:39 +0200

    Add task 2 completed check security condition

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list (my_list, idx, new_element):
+    for i in range(len(my_list)):
+        if idx == -1 or idx >=(len(my_list)):
+            return None
+        if i == idx:
+            my_list[i] = new_element
+            return my_list
