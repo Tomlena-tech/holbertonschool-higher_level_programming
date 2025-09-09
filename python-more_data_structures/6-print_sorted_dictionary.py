d4893194ac2b27b3a606e6c8eaf5dadff2078c92
Tomlena-tech <decourttthomas@gmail.com>
2025-09-09 13:55:51 +0200

    Add task 6 part 1

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+print(a_dictionary['Number'])
+print(a_dictionary['ids'])
+print(a_dictionary['language'])
+print(a_dictionary['track'])
