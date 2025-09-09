a3f21d593c23abb275fc8ff582edbbb88124bc97
Tomlena-tech <decourttthomas@gmail.com>
2025-09-09 15:57:27 +0200

    Add task 12 completed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
-        return 0
-    
-    
+
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        if char not in roman_numerals:
+            return 0
+        value = roman_numerals[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
