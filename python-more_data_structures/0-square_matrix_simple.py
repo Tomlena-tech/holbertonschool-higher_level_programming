d64cc3c3dc678f86bc527f3ee42fbd66517567b8
Tomlena-tech <decourttthomas@gmail.com>
2025-09-09 11:51:08 +0200

    Add task 0 part 0 lol

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-new_matrix = []
-square_matrix_simple = []
-for row in matrix:
+    new_matrix = []
+    for row in matrix:
     map(lambda x: new_matrix.append(x * x), row)
     square_matrix_simple.append(new_matrix)
-    new_matrix = []
+        new_matrix = []
 return square_matrix_simple
