690114ddc5bd863dcdeb6d2d9e76dc7a029d5138
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 13:10:06 +0200

    Add task 3 completed

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    if a is None or b is None:
+        return None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
