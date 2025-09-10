0e4ec8954e4597223f5fd09da5119ada2f695b15
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 14:20:37 +0200

    update task 0 cancel first if None etc

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list is None or x < 0:
-        print()
-        return 0
     count = 0
     if x > len(my_list):
         x =len(my_list)
