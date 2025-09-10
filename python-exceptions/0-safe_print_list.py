6b84972f8534771708d5682ba39e7686923b130a
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 12:13:22 +0200

    Add task 0 completed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-if my_list is None or x < 0:
-    print()
-    return 0
+    if my_list is None or x < 0:
+        print()
+        return 0
     count = 0
-    for i in range(my_list)
+    if x > len(my_list):
+        x =len(my_list)
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
+    
     
