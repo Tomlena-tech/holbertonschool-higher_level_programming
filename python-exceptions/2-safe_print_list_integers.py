816c4de006532a2da80fc078345d7524d9fcbc55
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 12:47:41 +0200

    Add task 2 completed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None or x < 0:
+        return 0
     count = 0
-    for value  in my_list[:x]:
+    for value in my_list[:x]:
         try:
             print("{:d}".format(value), end="")
-                except ValueError:
-                continue
-                except TypeError:
-                continue
+            count += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print()
+    return count
+        
