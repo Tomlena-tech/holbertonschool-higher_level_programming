6f111c99b8d573786ae1ea2fc006840ff14cfa04
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 13:33:22 +0200

    Add task 4 part 0

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    if my_list_1 is None or my_list_2 is None or len(list_length <= 0):
+        return []
+    result = []
