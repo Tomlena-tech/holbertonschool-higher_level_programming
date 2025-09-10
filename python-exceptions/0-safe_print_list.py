a3b220fdbe5ad3e45a5926e26fd9ded090e8ebee
Tomlena-tech <decourttthomas@gmail.com>
2025-09-10 11:03:33 +0200

    Add task 0 part 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+if my_list is None or x < 0:
+    print()
+    return 0
+    count = 0
+    for i in range(my_list)
+    
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+Exceptions files for Python
