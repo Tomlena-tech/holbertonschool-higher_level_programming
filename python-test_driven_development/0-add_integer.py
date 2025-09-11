961b4e557d82c2434cf9542c5d933ab8a517737a
Tomlena-tech <decourttthomas@gmail.com>
2025-09-11 09:58:34 +0200

    Add task 0 part 0 creation files

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Return the sum of a and b as integers.
+
+    Args:
+        a (int, float): The first number.
+        b (int, float, optional): The second number. Defaults to 98.
