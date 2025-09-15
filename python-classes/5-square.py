8c24a879ff99d8e3e170c63c7ea74f4bc450ce26
Tomlena-tech <decourttthomas@gmail.com>
2025-09-15 15:57:38 +0200

    Add task 5 part 1

---
+++ python-classes/5-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module 5-square.py that defines a square by: (based on 4-square.py) """
+class Square:
+    """Defines a square with size and area methods, and a my_print method."""
+    def __init__(self, size=0):
+        """"initialise the data"""
+        self.size = size
+        @property
+        def size (self):
+            return self.__size
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+            def area(self):
+                """Return the current square area."""
+                return self.__size * self.__size
+            def my_print(self):
+                """Print the square with the character #."""
+                if self.__size == 0:
+                    print("")
+                for i in range(self.__size):
+                    print("#" * self.__size)
