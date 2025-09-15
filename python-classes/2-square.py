40f770a39fe5707cb6ef873133c54d27a251ea95
Tomlena-tech <decourttthomas@gmail.com>
2025-09-15 15:16:15 +0200

    update task 2 indentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
-def __init__(self, size=0):
-    """Initialize a new Square.
-    Args:
+class Square:
+    """A class that defines a square by its size."""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
         size (int): The size of the new square.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
