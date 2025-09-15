bbfa5e99d46a1e4347c7308454ced617a5f13530
Tomlena-tech <decourttthomas@gmail.com>
2025-09-15 13:36:25 +0200

    Add task 1 part 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,3 +8,11 @@ class Rectangle:
         if type value != int:
             raise TypeError("width must be an integer")
         return self.__width = value
+    def height(self):
+        return self__height
+    def height(self, value):
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if type value != int:
+            raise TypeError ("height must be an integer")
+        return self.__height = value
