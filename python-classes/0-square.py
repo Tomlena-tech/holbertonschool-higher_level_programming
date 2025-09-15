efba6377a9487760b4a485243bb5a7d0e2114c69
Tomlena-tech <decourttthomas@gmail.com>
2025-09-15 14:51:10 +0200

    Add task 0 with comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
 class Square:
+    """Represents a square.
+    
+    This is an empty class that defines a square.
+    No attributes or methods are implemented yet.
+    """
     pass
