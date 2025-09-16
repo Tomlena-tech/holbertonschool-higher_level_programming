b0c7d847fc5dacb64dff372df39b1123ab7e161c
Tomlena-tech <decourttthomas@gmail.com>
2025-09-16 16:00:13 +0200

    Add task 2 part 1

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" Defines a Rectangle class with width and height properties, and methods to calculate area and perimeter.
+"""
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
