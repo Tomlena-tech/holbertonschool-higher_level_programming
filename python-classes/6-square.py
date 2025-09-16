2c5ac8e2b506d5b497ade6b096e51b938bf41f9b
Tomlena-tech <decourttthomas@gmail.com>
2025-09-16 10:50:18 +0200

    Add task 6 part 1

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,3 +5,25 @@ class Square:
         """"initialise the data"""
         self.size = size
         self.position = position
+        
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance (value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    @property
+    def position(self):
+        return(self.__position)
+    
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
