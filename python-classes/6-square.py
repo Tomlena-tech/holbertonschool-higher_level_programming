cca105cc3bd7e96b3cda93a58bb9f836ea09a26e
Tomlena-tech <decourttthomas@gmail.com>
2025-09-16 09:59:26 +0200

    Add task 6 part 0

---
+++ python-classes/6-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module 6-square.py that defines a square by: (based on 5-square.py)"""
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        """"initialise the data"""
+        self.size = size
+        self.position = position
