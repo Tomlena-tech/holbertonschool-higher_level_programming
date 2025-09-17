4ba4447fc481138b2494f9298cbeb141442a519c
Tomlena-tech <decourttthomas@gmail.com>
2025-09-17 11:40:53 +0200

    Add task 3 part 0

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a Rectangle class with width and height properties, and methods to calculate area and perimeter.
+"""
+class Rectangle:
+    """Defines a rectangle by width and height."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+   
