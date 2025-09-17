cbfa01aabe5a3b9653256541931b29669b6bad41
Tomlena-tech <decourttthomas@gmail.com>
2025-09-17 11:00:19 +0200

    Add task 2 part 1

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -2,14 +2,40 @@
 """ Defines a Rectangle class with width and height properties, and methods to calculate area and perimeter.
 """
 class Rectangle:
+    """Defines a rectangle by width and height."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         
     @property
     def width(self):
+        """gets the width of the rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
-        if width < 0:
+        """sets the width of the rectangle with validation"""
+        if value < 0:
             raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__width = value
+    @property
+    def height(self):
+        """gets the height of the rectangle"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """sets the height of the rectangle with validation"""
+        if value < 0: 
+            raise ValueError("height must be >= 0")
+        if not isinstance (value, int):
+            raise TypeError("height must be an integer")
+        self.__height = value
+     
+    def area(self): 
+        """Defines the area of the redctangle"""
+        return self.__width * self.__height
+    def perimeter(self):
+        """ Defines parameter of the rectangle"""
+    
