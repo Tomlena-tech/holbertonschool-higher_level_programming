62f1a0e1431c5dd087fbe960ed4b9249cba606d4
Tomlena-tech <decourttthomas@gmail.com>
2025-09-17 13:16:22 +0200

    Add correction task 3 str function deleted & create task 4 part 0

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -45,11 +45,3 @@ class Rectangle:
         """Returns the rectangle as a string of # characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rect = ""
-        for i in range(self.__height):
-            rect += "#" * self.__width
-            if i != self.__height - 1:
-                rect += "\n"
-        return rect
- 
-    
