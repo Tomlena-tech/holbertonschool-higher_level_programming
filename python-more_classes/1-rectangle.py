20be6a98e80d2685d47fb45d1b7197455a0e9145
Tomlena-tech <decourttthomas@gmail.com>
2025-09-17 10:09:37 +0200

    Add comments task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-class Rectangle:
-    
+    """Defines a Rectangle class with width and height properties, and methods to calculate area and perimeter.
+    """
+    class Rectangle:
+    """ Represents a rectangle with width and height.
+    """
+
     def __init__(self, width=0, height=0):
+        """ Initialize a Rectangle instance."""
         self.width = width
         self.height = height
         
     def width(self):
+        """ Getter for width"""
         return self.__width
     
     def width(self, value): 
+        """ Setter for width"""
         if value < 0:
             raise ValueError("width must be >= 0")
         if type (value) != int:
@@ -17,9 +24,11 @@ class Rectangle:
     
     
     def height(self):
+        """getter for height"""
         return self.__height
     
     def height(self, value):
+        """setter for height"""
         if value < 0:
             raise ValueError("height must be >= 0")
         if type (value) != int:
