18f05e586643a589fdc649cf9b531e85502e3904
Tomlena-tech <decourttthomas@gmail.com>
2025-09-17 15:27:39 +0200

    Add task 8 part 1

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -67,3 +67,9 @@ class Rectangle:
         """Deleting an instance"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2)
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instanceof Rectangle")
