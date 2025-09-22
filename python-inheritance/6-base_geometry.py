8472f3dfdca750e2cff3a7a1aa9277bbd9dcf68d
Tomlena-tech <decourttthomas@gmail.com>
2025-09-22 15:25:00 +0200

    Add task 6 whole

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+    
+    def area(self):
+        """Raises an Exception with message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
