408ea480f20b958ff275d11739fadeaef9e3cb48
Tomlena-tech <decourttthomas@gmail.com>
2025-09-22 14:33:42 +0200

    Add comments under shebang

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines a function who returns the object / instance / class true or false""" 
 def is_same_class(obj, a_class):
     """
     Returns True if obj is exactly an instance of a_class, otherwise False
