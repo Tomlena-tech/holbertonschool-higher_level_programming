7bb586651a9cdbcb984f1004fcbd215aab1b230b
Tomlena-tech <decourttthomas@gmail.com>
2025-09-22 13:27:18 +0200

    Add correction task 0 comments & indentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 """Defines how do you print attributes and methods of an objects"""
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: List of attribute and method names.
+    """
     return dir(obj)
     
