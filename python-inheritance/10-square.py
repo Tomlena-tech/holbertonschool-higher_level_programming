4f74a10ec2f46bb22be73cd1f74464ab32c14795
Tomlena-tech <decourttthomas@gmail.com>
2025-09-23 14:36:33 +0200

    modify task 10 part 1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-""" Write a square class inherits from Rectangle"""
-
+"""
+Module 10-square
+Définit la classe Square qui hérite de Rectangle
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
-    
-    def __init__(self, size): 
-        """Initialyse a square with size"""
-        
-        
+    """Classe Square héritant de Rectangle"""
+
+    def __init__(self, size):
+        """
+        Constructeur de Square
+
+        Args:
+            size (int): taille du carré
+        """
         self.integer_validator("size", size)
+        self.__size = size
+        # Appel du constructeur de Rectangle avec size pour width et height
         super().__init__(size, size)
-    
+
+    def area(self):
+        """Retourne l’aire du carré"""
+        return self.__size * self.__size
