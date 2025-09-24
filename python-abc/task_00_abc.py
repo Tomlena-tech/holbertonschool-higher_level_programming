f6eef8507a792ab419916f9be7953a6e8d70a90b
Tomlena-tech <decourttthomas@gmail.com>
2025-09-24 14:05:09 +0200

    Add task 0 part 1 ?

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env/python3
+"""Set up and implement Abstract class"""
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    pass
+    @abstractmethod
+    def sound(self):
+            pass
+        
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+    
+if __name__ == "__main__":
+    print("=== Test des animaux ===")
