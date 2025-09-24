7e5bd3f9108ca3c0bd464cee6aa9202f5b2bd0ae
Tomlena-tech <decourttthomas@gmail.com>
2025-09-24 15:06:48 +0200

    Add task 1 part 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """History around the DuckShape with area and perimeter !!!"""
 from abc import ABC, abstractmethod
-
+import math
 
 class Shape(ABC):
     @abstractmethod
@@ -10,3 +10,26 @@ class Shape(ABC):
     
     def perimeter(self):
         pass
+    
+class Circles(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+class Rectangle(Shape): 
+    def __init__(self, height, width):
+        self.width = width
+        self.height = height
+        
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 * (self.width + self.height)    
+    
+    def shape_info(self):
+        """Return information about the shape"""
+        return f"This is a {self.__class__.__name__}"
