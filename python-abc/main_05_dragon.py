a182b5e38a16eb7d5fdb05553287bfcc540d02db
Tomlena-tech <decourttthomas@gmail.com>
2025-09-25 21:40:34 +0200

    Add correction / checking task 5

--- python-abc/main_05_dragon.py
+++ python-abc/main_05_dragon.py
@@ -5,3 +5,8 @@ dragon = Dragon()
 dragon.swim()  # Outputs: The creature swims!
 dragon.fly()   # Outputs: The creature flies!
 dragon.roar()  # Outputs: The dragon roars!
+
+print()
+print("Draco peut faire tout ça grâce aux mixins !")
+print(f"Classes parentes de Dragon: {Dragon.__bases__}")
+print(f"MRO (Method Resolution Order): {Dragon.__mro__}")
