a3e8278c3f8cf5e8176f990fbac3338c23544830
Tomlena-tech <decourttthomas@gmail.com>
2025-09-25 15:22:28 +0200

    Add task 3 full

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
-""" Build an iterator who gies elements one by one"""
+""" Build an iterator who gives elements one by one"""
+
+class CountedIterator:
+    """Create a class CountedIterator"""
+    
+    def __init__(self, iterable):
+        """Builder"""
+        self.iterator = iter(iterable)
+        self.count = 0
+        
+    def get_count(self):
+        """Defines counting """
+        return self.count
+    
+    def __next__(self):
+        """Next method for counting"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+    def __iter__(self):
+        """Pour que ça marche avec for loops"""
+        return self
