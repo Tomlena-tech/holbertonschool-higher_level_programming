103302ea18caaf231c4b13032b03b3e486360a0e
Tomlena-tech <decourttthomas@gmail.com>
2025-09-25 15:50:28 +0200

    Add task 4 part 1

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 """Buil a flying fish !!from Fish and Bird"""
+
+
+class Fish:
+    def swim(self):
+        print ("The fish is swimming")
+    
+    def habitat(self):
+        print ("The fish lives in water")
+        
+class Bird:
+    def fly(self):
+        print ("The bird is flying")
+    def habitat(self):
+        print("The bird lives in the sky")
+        
+
+
+
+class FlyingFish(Fish, Bird):
+
+    
