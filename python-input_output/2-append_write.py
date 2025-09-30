ddc866dda6ef6c9973e866b18bbed43269af61cf
Tomlena-tech <decourttthomas@gmail.com>
2025-09-30 22:52:56 +0200

    Add task 2 part 1 ?

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,4 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    
+    """Add a string a the end of a string and count the number of character"""
+    with open(filename, 'a', encoding= 'utf-8') as f:
+        return f.write(text)
