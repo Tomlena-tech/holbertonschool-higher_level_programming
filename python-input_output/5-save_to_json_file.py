ef73d87756aee810e82b3188062bb46fa2142314
Tomlena-tech <decourttthomas@gmail.com>
2025-09-30 23:49:58 +0200

    Add task 5 part 2 ?

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,4 +5,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Function to save python object into json file"""
     with open(filename, 'w' ,encoding='utf-8') as f:
-        json.dumb(my_obj, f)
+        json.dump(my_obj, f)
