66c70b332ce12aa2c65ee0a2cef4ac9e719f4ab8
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 00:41:57 +0200

    Add task 7 part 2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,4 +7,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     my_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    my_list = []    
+    my_list = [] 
+       
+my_list.extend(sys.argv[1:])
+"""Add arguments"""
+
+"""Save it"""
+save_to_json_file(my_list, "add_item.json")
