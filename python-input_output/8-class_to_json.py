edcd5aa3da422d31c0303f9682d599c48950bf5e
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 01:00:33 +0200

    Add task 8 not far from beginning..

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 """ Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object """
 import sys
+
+
+def class_to_json(obj):
+    """Return the description in dictionnary of an object for Json"""
+    return obj.__dict__
