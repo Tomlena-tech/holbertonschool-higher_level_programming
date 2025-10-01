a534ac933783d23d647627cf49b387e672012b41
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 22:47:47 +0200

    Add task 2 part 2 full ?

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""Module for converting CSV data to JSON format"""
 
 
 import csv
 import json
 
 def convert_csv_to_json(csv_filename):
+    """Convert a CSV file to JSON format and save to data.json"""
     try:
          
         with open(csv_filename, 'r') as csv_file:
@@ -13,10 +15,11 @@ def convert_csv_to_json(csv_filename):
         with open('data.json', 'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=4)
         return True
+    
     except FileNotFoundError:
-        # Return False if file not found
+        
         return False
     
     except Exception:
-        # Return False for any other errors
+        
         return False
