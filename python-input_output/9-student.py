c39d0139bb96ee64079898213dff293464f7845e
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 11:13:22 +0200

    Add task 9 part 0

---
+++ python-input_output/9-student.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+""" Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object """
