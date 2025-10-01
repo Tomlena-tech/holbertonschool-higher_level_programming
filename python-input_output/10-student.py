07698fb6030e453626660254c2a5085f207ddfd2
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 12:26:31 +0200

    Add task 10 part 1

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student:
+    """Class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance
+        
+        Args:
+            attrs: List of attribute names to retrieve (optional)
+                   If None, all attributes are retrieved
+        
+        Returns:
+            A dictionary containing the specified attributes
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        
+        return self.__dict__
