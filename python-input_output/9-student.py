95ec7e4b2e82d7d484c7ffb973059f840e9c80a5
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 11:53:46 +0200

    Add correct version

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
 class Student:
     """Class that defines a student"""
 
-
     def __init__(self, first_name, last_name, age):
         """Initialize a Student instance"""
-
-        self.last_name = last_name
         self.first_name = first_name
+        self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """Retrieve a dictionary representation of a Student instance"""
-        """A dictionary containing all instance attributes"""
         return self.__dict__
