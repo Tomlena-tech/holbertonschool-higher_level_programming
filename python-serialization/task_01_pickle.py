2d4a86d7da646ed19dd4230f1105537e9bb65704
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 22:00:29 +0200

    Add task 1 part 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -5,3 +5,24 @@
 class CustomObject:
     def __init__(self, name, age, is_student):
         """Build CustomObject"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    
+    def display(self):
+        """Displaying attributs"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}") 
+        print(f"Is_student: {self.Is_student}") 
+    
+    def serialize(self, filename):
+        """Serialize file"""
+        try:
+          with open(filename, 'wb') as file:
+              pickle.dump(self, file) 
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
