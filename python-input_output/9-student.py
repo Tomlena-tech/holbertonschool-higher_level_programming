2e47387f01bc5102bb8e69aaeb0cea68bb007c60
Tomlena-tech <decourttthomas@gmail.com>
2025-10-01 11:39:13 +0200

    Add task 9 part 1

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,3 +5,8 @@ class Student:
         """Initialize a Student instance"""
         
     
+        self.last_name = last_name
+        self.first_name = first_name
+        self.age = age
+        
+    
