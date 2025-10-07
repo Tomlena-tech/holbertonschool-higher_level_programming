b7c45571d5aeac946c35b5057d04254b2a356868
Tomlena-tech <decourttthomas@gmail.com>
2025-10-07 11:25:39 +0200

    Add correction mzin for task 2

--- restful-api/main_02_requests.py
+++ restful-api/main_02_requests.py
@@ -3,5 +3,6 @@
 from task_02_requests import fetch_and_print_posts, fetch_and_save_posts
 
 if __name__ == "__main__":
+    """fetch and print"""
     fetch_and_print_posts()
     fetch_and_save_posts()
