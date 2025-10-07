04eff271cb946046f9b2af8807adcad1927027bd
Tomlena-tech <decourttthomas@gmail.com>
2025-10-07 15:28:26 +0200

    Add data / elif / else 404

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,10 +8,26 @@ import json
 class Simple_server(BaseHTTPRequestHandler):
     """Creta a subsclass of of BaseHTTPRequestHandler"""
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+            
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"undefined endpoint")
+   
+
         
 if __name__ == "__main__":
     PORT = 8000
