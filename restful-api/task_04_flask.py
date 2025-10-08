2f4384f3cbdd72a86e56c1f72934608bc64beb67
Tomlena-tech <decourttthomas@gmail.com>
2025-10-08 13:39:27 +0200

    Add task 4 part 0,5

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,3 +2,16 @@
 """ Develop a simple API using Python with the Flask framework"""
 
 
+import json
+
+from flask import Flask
+app = Flask(__name__)
+
+def home():
+     if self.path == "/" or self.path == "":
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Welcome to the Flask API!")
+        
+if __name__ == "__main__": app.run()        
