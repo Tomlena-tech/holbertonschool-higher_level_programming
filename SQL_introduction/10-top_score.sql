0d609a174e37ab6a649c98652cc8dcda6e28ccb1
Tomlena-tech <decourttthomas@gmail.com>
2025-10-21 11:43:29 +0200

    Add All task from 0

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+SELECT FROM second_table ORDER BY score DESC LIMIT 1;
+-- Select the row with the highest score from second_table
---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,1 @@
+UPDATE second_table SET score = score - 4 WHERE name = 'Bob';
