639c4a334dc3d5e431e7e2f210e702e8b889b554
Tomlena-tech <decourttthomas@gmail.com>
2025-10-23 13:19:10 +0200

    Add task 0 from part 2

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,27 @@
-### Listing all states from USA database ###
+""" Listing all states from USA database """
 #!/usr/bin/env python3
 import MySQLdb
 import sys
 
+
+def list_states(user: str, password: str, db_name: str) -> None:
+    """Se connecte et affiche les états."""
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
+
+# Protection « pas d'exécution à l'import »
+if __name__ == "__main__":
+    # Récupération des 3 arguments tels quels
+    user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(user, password, db_name)
