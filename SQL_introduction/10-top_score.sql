ba28f9384632d2964207a9566e084ddba38b8838
Tomlena-tech <decourttthomas@gmail.com>
2025-10-23 09:54:44 +0200

    correction task 10 exo 0

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Liste tous les enregistrements de second_table triés par score décroissant
-SELECT FROM second_table ORDER BY score DESC LIMIT 1;
+SELECT score, name FROM second_table ORDER BY score DESC;
 
