8ab4f100c125850acb2c0a71c01333010db04c55
Tomlena-tech <decourttthomas@gmail.com>
2025-10-25 22:25:54 +0200

    Add task 2 part 1

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Module qui permet de récupérer une liste de ville des
+USA commencant par la lettre N.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    cursor = db.cursor()
+    
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+    
+    for row in cursor.fetchall():
+        print(row)
+    
+    cursor.close()
+    db.close()
