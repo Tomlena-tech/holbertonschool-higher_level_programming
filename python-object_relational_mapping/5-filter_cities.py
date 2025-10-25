c5efda60bbd34f6b37c736ee194561e5bc290e2a
Tomlena-tech <decourttthomas@gmail.com>
2025-10-26 00:01:02 +0200

    Add task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+
+#!/usr/bin/python3
+
+"""
+Module qui permet de récupérer une liste de ville des
+USA ainsi que des les afficher à l'écran.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(query, (state_name,))
+    cities = [row[0] for row in cursor.fetchall()]
+    print(", ".join(cities))
+    cursor.close()
+    db.close()
