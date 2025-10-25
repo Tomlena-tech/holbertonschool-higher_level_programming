46edee09821d1e7c45c2ab4fd521a29f705d4815
Tomlena-tech <decourttthomas@gmail.com>
2025-10-25 22:54:52 +0200

    More changings db_base = database

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
