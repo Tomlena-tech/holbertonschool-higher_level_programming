abb58bdbed4796f02426febf061e0f2d4d8b131e
Tomlena-tech <decourttthomas@gmail.com>
2025-10-25 23:55:03 +0200

    new version of task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,18 +4,33 @@
 Module qui permet de récupérer une liste de ville des
 USA ainsi que des les afficher à l'écran.
 """
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=dbname)
-    cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name "
-                "FROM cities c JOIN states s ON c.state_id = s.id "
-                "ORDER BY c.id ASC")
-    for row in cur.fetchall():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
