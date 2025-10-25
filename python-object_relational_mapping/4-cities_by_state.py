8969d117c023d417177c043f2a0d18c4d9477bd6
Tomlena-tech <decourttthomas@gmail.com>
2025-10-25 23:25:54 +0200

    Add task 4 part 1

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module qui permet de récupérer une liste de ville des
+USA ainsi que des les afficher à l'écran.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
