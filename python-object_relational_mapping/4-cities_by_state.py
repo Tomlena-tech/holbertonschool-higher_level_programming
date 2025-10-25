eb08f29c52cdd43dfdc13da55db4520f98a9ac5f
Tomlena-tech <decourttthomas@gmail.com>
2025-10-25 23:35:11 +0200

    Add settings

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,21 +7,30 @@ import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+   
+   
     # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
     # Create cursor object
     cursor = db.cursor()
 
     # Execute SQL query with JOIN to get cities with their state names
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC")
 
     # Fetch and display all results
     for row in cursor.fetchall():
