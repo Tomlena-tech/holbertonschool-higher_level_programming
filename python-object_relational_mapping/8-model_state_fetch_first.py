a42e37fd68c02e8df95fde7f9c5c96377be222cb
Tomlena-tech <decourttthomas@gmail.com>
2025-10-26 01:19:16 +0200

    add task 8 part 3

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """
-Module qui permet de récupérer la première ville de la
-database.
+Print the first State object from hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    first_state = session.query(State).order_by(State.id).first()
+    first = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(first.id, first.name)) if first else print("Nothing")
 
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
-        print("Nothing")
+    session.close()
