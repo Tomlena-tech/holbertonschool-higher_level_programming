d7fb6a3059b956e6d61ea4c3804a71f4fe74a910
Tomlena-tech <decourttthomas@gmail.com>
2025-11-06 12:57:19 +0100

    Add curly parenthesis

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const count = process.argv.length - 2;
 
-if(count === 0)
+if(count === 0){
     console.log('No argument');
-else if(count === 1)
+}else if(count === 1){
     console.log('Argument found');
-else
+}else{
     console.log('Arguments found');
+}
