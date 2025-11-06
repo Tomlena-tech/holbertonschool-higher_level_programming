4b4dae192e18dcf7cbaeb3f7001b0828b7c705fb
Tomlena-tech <decourttthomas@gmail.com>
2025-11-06 21:46:06 +0100

    add task 10 3rd time..

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-function factorial(n) {
+
+function factorial (n) {
   if (isNaN(n)) return 1;
-    if (n === 0) return 1;
+  if (n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
