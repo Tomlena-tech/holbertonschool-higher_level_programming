3310cbdfa28180b45ae2e52b2a7e1d6c9196e23f
Tomlena-tech <decourttthomas@gmail.com>
2025-11-13 13:50:03 +0100

    Add function to html files

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/header')
+def header()
+    return render_template('header') 
+
+@app.route('footer')
+def footer()
+    return render_template('footer')
+
+@app.route('about')
+def about()
+    return render_template('about')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+@app.route('/items')
